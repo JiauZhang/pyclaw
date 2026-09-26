ff3df07e2546313829cc35370a606caff8c6163e
JiauZhang <1743960454@qq.com>
2026-09-26 16:06:50 +0800

    refactor: drop the four information tools that are not ours to offer

--- pyclaw/tools/__init__.py
+++ pyclaw/tools/__init__.py
@@ -4,14 +4,6 @@ from .bash import Bash
 from .edit import Edit, Write
 from .glob import Glob
 from .grep import Grep
-from .info import datetime_tool, geocode_tool, location_tool, weather_tool
 from .read import Read
 
 BUILTIN_TOOLS = (Read, Glob, Grep, Write, Edit, Bash, TaskOutput, TaskStop)
-
-tools = [
-    datetime_tool,
-    geocode_tool,
-    location_tool,
-    weather_tool,
-]
